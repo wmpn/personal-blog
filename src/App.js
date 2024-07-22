1f68056c42bd3368c68d312373b558147e424263
praveenng <75559018+wmpn@users.noreply.github.com>
2024-07-22 12:53:28 +0530

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Blog</h1>
+      <div id='page-body'>
+        welcome to the blog
+      </div>
+    </div>
+  );
+}
+
+export default App;
